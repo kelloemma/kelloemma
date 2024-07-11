066a3d013712b006189c4e2fcabcb6c5161241f9
kelloemma <kelloemma@gmail.com>
2024-07-10 21:57:56 -0700

    post

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter as Router } from "react-router-dom";
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <>
-//     <App />
-//   </>,
-// )
-ReactDOM.render(
-  <Router basename="/kelloemma">
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
     <App />
-  </Router>,
-  document.getElementById("root")
-);
+  </>,
+)
+
